fa693f8c5992caf7ae2230dc828db7b886de0dd4
Eduardo <edu.goes10@gmail.com>
2026-02-10 15:09:20 -0300

    first commit

---
+++ server/routes/main.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+
+// Routes
+router.get("", (req, res) => {
+    const locals = {
+        title: "NodeJS Blog",
+        description: "Simple blog created with NodeJS, Express, EJS and MongoDB"
+    }
+
+
+    res.render('index', { locals });
+});
+
+router.get("/about", (req, res) => {
+    res.render('about');
+});
+
+module.exports = router;
